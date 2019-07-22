e5fac4afb6e3ea7ccdb43e147cf98f03cc7cce8e
David Jermaine Whitehead <david_whitehead_234@yahoo.com>
2019-07-22 20:20:00 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -23,7 +23,7 @@ countdown_output = 10
   while countdown_output <= 10
    puts "#{countdown_output} SECOND(S)!"
    countdown_output -= 1
-   sleep 5
+   sleep(5)
    break if countdown_output == 0
    
   end
